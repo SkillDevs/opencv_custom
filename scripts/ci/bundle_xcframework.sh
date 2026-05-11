6f029a8466380fe516d799667cd0e90a2dda946c
David Martos <davidmartos96@gmail.com>
2026-05-11 10:43:09 +0200

    bundle framework

--- scripts/ci/bundle_xcframework.sh
+++ scripts/ci/bundle_xcframework.sh
@@ -4,6 +4,23 @@ set -eux
 
 XCFRAMEWORK=opencv2.xcframework
 
+function edit_xcframework() {
+    for i in $( ls $XCFRAMEWORK ); do
+        if [ -d $XCFRAMEWORK/$i ]; then
+            echo $i
+
+            FOLD="$XCFRAMEWORK/$i/opencv2.framework"
+
+            # without this patch, the local framework works, but not the SPM binary package containing it
+            plutil -insert CFBundleExecutable -string opencv2 ${FOLD}/Versions/A/Resources/Info.plist
+
+            # The resources in Versions/A are the correct ones and we move them to the root
+            cp $FOLD/Versions/A/Resources/* $FOLD/Resources
+        fi
+    done
+
+}
+
 function fix_shallow() {
     platform=$1
     pushd $XCFRAMEWORK/$platform/opencv2.framework
@@ -27,6 +44,7 @@ xcodebuild -create-xcframework \
     -framework build/framework-macos/opencv2.framework \
     -output $XCFRAMEWORK
 
+edit_xcframework
 fix_shallow ios-arm64
 fix_shallow ios-arm64_x86_64-simulator
 
